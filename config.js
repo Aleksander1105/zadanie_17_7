db284d4a8322bc405b9f245ef539bd2618613791
Aleksander Matyjewicz <matyjewiczaleksander@gmail.com>
2018-05-05 00:00:32 +0200

    Zadanie_17_7(basic)-DONE!

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '573839478983-j5k8129l6sgts3fr3njl2hr13595794f.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'rr3osBkmRjUPMFQROhgvY2jb',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
